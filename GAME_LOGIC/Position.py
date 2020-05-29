e20908393a437082764b88519ff1fbd0cc2d282d
ReQezeR <mp62639@o2.pl>
2020-05-29 14:41:02 +0200

    add Position class

--- GAME_LOGIC/Position.py
+++ GAME_LOGIC/Position.py
@@ -1,1 +1,16 @@
-# TODO: Position class
+class Position:
+    def __init__(self, y=None, x=None):
+        self.y = y
+        self.x = x
+
+    def getX(self):
+        return self.x
+
+    def setX(self, x):
+        self.x = x
+
+    def getY(self):
+        return self.y
+
+    def setY(self, y):
+        self.y = y
